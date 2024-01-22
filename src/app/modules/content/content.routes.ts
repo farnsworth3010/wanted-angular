08de31b77e9339855be49a752a807e5a0eff0911
scemer <farnsworth3010@proton.me>
2024-01-22 11:17:21 +0300

    fix: remove lazyload wanted (caused long loading)

--- src/app/modules/content/content.routes.ts
+++ src/app/modules/content/content.routes.ts
@@ -3,6 +3,7 @@ import { SettingsComponent } from './settings/settings.component';
 import { ContentComponent } from './content.component';
 import { WantedComponent } from './wanted/wanted.component';
 import { HomeComponent } from './home/home.component';
+import { wantedRoutes } from './wanted/wanted.routes';
 
 export const contentRoutes: Routes = [
   {
@@ -18,7 +19,8 @@ export const contentRoutes: Routes = [
       },
       {
         path: 'crimes',
-        loadChildren: () => import('./wanted/wanted.routes').then(m => m.wantedRoutes),
+        children: wantedRoutes,
+        // loadChildren: () => import('./wanted/wanted.routes').then(m => m.wantedRoutes),
         component: WantedComponent,
         data: {
           animation: 'crimesPage',
