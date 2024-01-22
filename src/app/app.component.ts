f88854177179190c2617a5c0366ba195e572a7d9
scemer <farnsworth3010@proton.me>
2024-01-22 22:57:36 +0300

    feat: dark theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { ChangeDetectionStrategy, ChangeDetectorRef, Component, DestroyRef, Host
 import { RouterOutlet } from '@angular/router';
 import { SettingsService } from './core/services/settings.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { OverlayContainer } from '@angular/cdk/overlay';
 
 @Component({
   selector: 'app-root',
@@ -16,14 +17,15 @@ export class AppComponent implements OnInit {
   constructor(
     private settingsService: SettingsService,
     private destroyRef: DestroyRef,
-    private changeDetectorRef: ChangeDetectorRef
+    private changeDetectorRef: ChangeDetectorRef,
+    private overlayContainer: OverlayContainer
   ) {}
 
   animations: boolean = true;
-  isDark: boolean = false
+  isDark: boolean = false;
 
   @HostBinding('class') get themeMode() {
-    return this.isDark ? 'theme-dark' : 'theme-light'
+    return this.isDark ? 'theme-dark' : 'theme-light';
   }
   @HostBinding('@.disabled') get animationsDisabled() {
     return !this.animations;
@@ -36,6 +38,14 @@ export class AppComponent implements OnInit {
     });
     this.settingsService.$themeState.pipe(takeUntilDestroyed(this.destroyRef)).subscribe((res: boolean) => {
       this.isDark = res;
+      const overlayContainerClasses = this.overlayContainer.getContainerElement().classList;
+      const themeClassesToRemove = Array.from(overlayContainerClasses).filter((item: string) =>
+        item.includes('theme-')
+      );
+      if (themeClassesToRemove.length) {
+        overlayContainerClasses.remove(...themeClassesToRemove);
+      }
+      overlayContainerClasses.add(res ? 'theme-dark' : 'theme-light');
       this.changeDetectorRef.markForCheck();
     });
   }
